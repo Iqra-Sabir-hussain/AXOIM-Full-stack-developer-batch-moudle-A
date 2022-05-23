e271430d2b39a35fe839b4531ddfb2b53ae7ff20
Iqra-Sabir-hussain <105587100+Iqra-Sabir-hussain@users.noreply.github.com>
2022-05-23 05:07:51 -0700

    PROJECT 1
    DAY 2 WORK COMPLETE

--- Project 1/script.js
+++ Project 1/script.js
@@ -1,4 +1,70 @@
+//Retriving element from the DOM
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+function showError(input, message) {
+    // Get the parent element of the input field (.form-control)
+    const formControl = input.parentElement;
+    // Replace the class - add error
+
+   formControl.className = 'form-control error';
+   //samll button
+    const small = formControl.querySelector ('small');
+    //mesage appear
+    small.innerText=message;
+};
+//sucess call
+function showSucess(input)
+{
+    const formControl =input.parentElement;
+    formControl.className ='form-control success';
+};
+
+//creat evnt listener on submit button 
+form.addEventListener('submit',function (e)
+{
+    //stop reloading on submit button 
+    e.preventDefault();
+    if( username.value === '')
+       {
+    showError(username,'username is not requried ');
+       }
+    else
+       {
+        showSucess(username);
+       }
+//email
+    if( username.value === '')
+      {
+    showError(email,'email is not requried ');
+       }
+    else
+      {
+        showSucess(email);
+       }
+
+  // password
+       if( username.value === '')
+      {
+    showError(password,'Passwird is not requried ');
+       }
+    else
+      {
+        showSucess(password);
+       }
+
+       // password2
+       if( username.value === '')
+      {
+    showError(password2,'confirm email is not requried ');
+       }
+    else
+      {
+        showSucess(password2);
+       }
 
 
-
-
+});
